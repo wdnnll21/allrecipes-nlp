ac4b02884b6101838d56ad35b7ecf65f88b8ff61
wdnnll21 <wesleydonnell2022@u.northwestern.edu>
2020-03-03 15:50:04 -0600

    Transformations 1-4

--- Transform.py
+++ Transform.py
@@ -1,7 +1,8 @@
-from TransformMaps import meatToVeg
+from TransformMaps import meatToVeg, vegToMeat
 from StepParser import Phrase, Sentence
 from RecipeX import Recipe
 from ingredients import Ingredient
+from random import randint
 
 def TransformIngredient(recipe,ingr,ingrnew):
     for action in recipe.steps:
@@ -28,6 +29,8 @@ def AddIngredient(recipe, ingr, when):
                 action.ingrs.append(ingr.ingredient)
                 action.objects.append(ingr.ingredient)
                 break
+    elif when == "none":
+        "Hello"
     else:
         newph = Phrase("add")
         newph.typ = "Combine"
@@ -76,14 +79,53 @@ def FromVegetarian(recipe):
                 TransformIngredient(recipe,ingredient,ingrnew)
                 addedMeat = True
     if not addedMeat:
-        AddIngredient()#add random meat
+        meat = list(meatToVeg)[randint(0,len(meatToVeg))]
+        AddIngredient(recipe,Ingredient(4,"ounces",meat),"with")
     
 
 def ToHealthy(recipe):
-    pass
+    ph = Phrase("trim")
+    for ingr in recipe.ingredients:
+        if any(meat in ingr.ingredient for meat in meatToVeg):
+            ph.objects.append(ingr.ingredient)
+            ph.ingrs.append(ingr.ingredient)
+        if any(sugaroil in ingr.ingredient for sugaroil in ["sugar","syrup","glaze","honey","oil","spray","lard","butter","fat","grease"]):
+            ingr.quantity /= 3
+        if "salt" in ingr.ingredient and "salted" not in ingr.ingredient:
+            TransformIngredient(recipe,ingr,Ingredient(ingr.quantity,ingr.measurement,"onion powder"))
+    if len(ph.objects) > 0:
+        AddAction(recipe,"begin",ph)
 
 def FromHealthy(recipe):
     ingrnew = [Ingredient(1,"tablespoon","salt"),Ingredient(1/4,"cup","butter")]
+    for ingrx in ingrnew:
+        AddIngredient(recipe,ingrx,"with")
+
+    unhealthy = ["bacon","tater tots","vegetable oil","marshmellow","hot dog","corn syrup","margarine","mayonnaise"]
+    i = 0
+    for ingr in recipe.ingredients:
+        if any(word in ingr.ingredient for word in ["carrot","fruit","bean","apple","lettuce","spinach","arugula","tomato","pea","kale","beet","cucumber","pear","banana"]):
+            TransformIngredient(recipe,ingr,Ingredient(ingr.quantity,"ounce",unhealthy[i%5]))
+
+    if not any(word in recipe.title for word in ["salad","sandwich"]):
+        AddIngredient(recipe,Ingredient(1,'cup','breading'),"none")
+        phone = Phrase("bread")
+        phone.typ = "Combine"
+        phone.preps.append("in a large bowl")
+        phone.tools.append("bowl")
+        phone.ingrs.append("breading")
+        ph = Phrase("deep fry")
+        ph.typ = "Cook"
+        ph.objects.append("everything")
+    
+    AddIngredient(recipe,Ingredient(50,None,'french fries'),"none")
+    ph = Phrase("serve")
+    ph.typ = "Wait"
+    ph.preps.append("with side of french fries")
+    ph.ingrs.append("french fries")
+    AddAction(recipe,ph,"end")
+
+    
 
 def ToLethal(recipe):
     ingrnew = Ingredient(2,"gallon","bleach")
